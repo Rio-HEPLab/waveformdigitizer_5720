8b7374f1155437b4799f06fff1279859a9a3b7fc
renne <backup2.rennelou@gmail.com>
2019-11-05 23:22:21 -0300

    funcionando

--- waveformAnaliser.py
+++ waveformAnaliser.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy.integrate import simps
 import ipdb
 
 XMIN = 450
@@ -19,9 +20,7 @@ def main():
     xmax=600
     conv = 1.0
 
-    #ipdb.set_trace()
-
-    signals = []
+    ipdb.set_trace()
 
     df['Baseline'] = 0
     df['Integral'] = 0
@@ -41,35 +40,10 @@ def main():
         df.loc[i,'Mean']  = mean(event,xmin,xmax,conv)
         df.loc[i,'Max']  = GetMax(event)
 
-    event = df.loc[0,'Vals']
-    xmin, xmax = findPulseWidth(event, 1/3)
-    xdata = np.arange(xmin, (xmax if xmax < event.size else event.size) ) * conv
-    event_r = event[xmin:xmax]
-    from scipy.integrate import simps
-    integral = simps(event_r, xdata)
-    print(integral)
-    print(df.loc[0,'Integral'])
-    popt, pcov = curve_fit(Gaus, xdata, event_r, p0=(1000.,500.,50.))
-    #print (popt)
-    #print (pcov)
+    fig, axes = plt.subplots(2)
 
-    popt_WithC, pcov_WithC = curve_fit(GausWithConstant, xdata, event_r, p0=(popt[0],popt[1],popt[2],0.))
-    #print (popt_WithC)
-    #print (pcov_WithC)
-
-    fig, axes = plt.subplots(2,2)
-    for i in range(5):
-        axes[0,0].plot( range(0, df.loc[i,'Vals'].size),df.loc[i,'Vals'] )
-
-    axes[0,1].hist( df['Mean'], bins=50 )
-
-    axes[1,0].plot( xdata, event_r, 'ko' )
-    #axes[1,0].plot( np.arange(event.size) ,event)
-    x_plot = np.linspace(xmin,xmax,200)
-    axes[1,0].plot( x_plot, Gaus( x_plot, A=popt[0], mean=popt[1], sigma=popt[2] ), 'r' )
-    axes[1,0].plot( x_plot, GausWithConstant( x_plot, A=popt_WithC[0], mean=popt_WithC[1], sigma=popt_WithC[2], C=popt_WithC[3] ), 'b' )
-
-    axes[1,1].hist( df['Integral'], bins=200, range=(0,10000) )
+    axes[0].hist( df['Mean'], bins=50 )
+    axes[1].hist( df['Integral'], bins=200, range=(0,10000) )
 
     plt.show()
 
@@ -85,9 +59,11 @@ def findPulseWidth(event, threshold):
         firstThreshold = XMIN
 
     enumerateEvent = list(zip(range(event[center : event.size].size), event[center : event.size]))
-    secondThreshold = bisearchRight(enumerateEvent, thresholdvalue) + center
+    secondThreshold = bisearchRight(enumerateEvent, thresholdvalue)
     if secondThreshold == None:
         secondThreshold = XMAX
+    else:
+        secondThreshold += center
 
     #assimetrico por causa do formato do pico ter maior tempo de queda que de subida
     return (firstThreshold - 10, secondThreshold + 150)
@@ -113,22 +89,15 @@ def bisearchRight(array, value):
         return bisearchRight(array[0 : x], value)
 
 def integrate(event, xmin, xmax, conv=1.0):
-    #x = np.arange(xmin, (xmax if xmax < event.size else event.size) ) * conv
-    #x = np.arange(0, event.size) * conv
     event_r = event[xmin:xmax]
-    return np.sum( event_r )
+    xdata = np.arange(xmin, (xmax if xmax < event.size else event.size) ) * conv
+    return simps(event_r, xdata)
 
 def mean(event, xmin, xmax, conv=1.0):
     x = np.arange(xmin, (xmax if xmax < event.size else event.size) ) * conv
     event_r = event[xmin:xmax]
     mean = np.sum( event_r * x ) / np.sum( event_r )
     return mean
-
-def Gaus(x, A, mean, sigma):
-   return A * np.exp( -0.5*( (x - mean)/sigma )**2 )
-
-def GausWithConstant(x, A, mean, sigma, C):
-   return A * np.exp( -0.5*( (x - mean)/sigma )**2 ) + C
 
 def GetBaseLine(event, _range):
     baseline = 0
